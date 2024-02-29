5d62ce6b0ac83dad873d7c9309f25d988ea88da7
routineforbots <138805602+routineforbots@users.noreply.github.com>
2024-02-29 16:46:31 +0100

    Update v2-FundMe.sol

--- lesson_4/v2-FundMe.sol
+++ lesson_4/v2-FundMe.sol
@@ -11,7 +11,18 @@ contract FundMe {
 
     uint256 public minimumUSD = 5e18;
 
+    address public owner; // used for constructor
+
+    // we want the onwer of the contract to be able to withdraw so that the withdraw function can be only called by the owner
+    // whenever this contract is deployed an owner is assigned to this contract with the following function build as constructior
+    // constructior is a keyword and special function in solidity which immidiatly called when the contract is deployed
+    constructor() {
+        owner = msg.sender;
+    }
+
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+
 
     function fund() public payable {
         //Since we made an attachment, we can call getConversionRate() in a different way:
@@ -25,6 +36,8 @@ contract FundMe {
 
 
    function withdraw() public {
+    require(msg.sender == owner, "Must be the owner!"); // from the constructor
+
     // we want to reset all the mappings 
     // for loop
     // [1, 2, 3, 4] elements
@@ -37,6 +50,7 @@ contract FundMe {
     // reset the array
     funders = new address[](0); // we are resetting array by creating new one with the length of 0
 
+    /* THEORY:
 
     // withdraw funds back to the function caller
     // there are 3 ways to do that
@@ -45,14 +59,15 @@ contract FundMe {
     // transfer can be used for transactions with gas less than 2300, otherwise it throws an error and the tx is reverted
 
     payable(msg.sender).transfer(address(this).balance ); // *this* keyword reffers to the whole contract FundMe
-    /* we are casting msg.sender with payable() so that
-    msg.sender which has type of *address* with the use of payable(msg.sender) will become *payable address*
-    */
+    // we are casting msg.sender with payable() so that
+    // msg.sender which has type of *address* with the use of payable(msg.sender) will become *payable address*
 
     // 2. send - has the same limit of 2300 for gas, but it returns boolean anyway
     bool sendSuccess = payable(msg.sender).send(address(this).balance);
     // but to revert transaction in case of error we need an additional line
     require(sendSuccess, "Send failed");
+
+    */
 
     // 3. call - recommended way; low level powerfull command which doesn't have gas limit; returns boolean
     (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}(""); // within "" we can call specific function
